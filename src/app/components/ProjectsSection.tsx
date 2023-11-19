355c80cf446c8553e8961526a79352c118bc98e3
poupsipoups <pauline.gobe16@gmail.com>
2023-11-19 22:47:53 +0100

    update projects

--- src/app/components/ProjectsSection.tsx
+++ src/app/components/ProjectsSection.tsx
@@ -7,7 +7,8 @@ const projectData = [
     {
         id: 1,
         title: "Shogureign",
-        description: "React • React Native • Javascript • HTML • CSS Git • GitHub • Agile Methodolog. Serious game for learning kanji. January 2023. Team of 4 people.Role: Game design and game architecture development in pairs.This project was carried out during a tutored project by Jérôme Hernandez. The specifications given were quite broad: create a mobile app game, Tinder-like (similar to the game Reigns), and integrating a system for learning Kanji characters. This project was completed with three other individuals, with whom we developed the game from A to Z (game story, game design, learning system, and development). The game is compatible with Android and iOS and was built using the React Native framework. The learning system is organized in two ways: The player learns kanji characters by encountering them in the game's story. The player has recurring lessons. These lessons have an evolving level based on the player's performance in exams (every 10 lessons).Furthermore, the player can review the encountered vocabulary, both in the learning phase and the acquired phase, in the dictionary.",
+        about: "Serious game for learning kanji. January 2023.",
+        description: "React • React Native • Javascript • HTML • CSS Git • GitHub • Agile Methodolog. Team of 4 people.Role: Game design and game architecture development in pairs.This project was carried out during a tutored project by Jérôme Hernandez. The specifications given were quite broad: create a mobile app game, Tinder-like (similar to the game Reigns), and integrating a system for learning Kanji characters. This project was completed with three other individuals, with whom we developed the game from A to Z (game story, game design, learning system, and development). The game is compatible with Android and iOS and was built using the React Native framework. The learning system is organized in two ways: The player learns kanji characters by encountering them in the game's story. The player has recurring lessons. These lessons have an evolving level based on the player's performance in exams (every 10 lessons).Furthermore, the player can review the encountered vocabulary, both in the learning phase and the acquired phase, in the dictionary.",
         image: "/images/projects_view/shogureign.png",
         tag: ["All", "Web", "Mobile"],
         gitUrl: "/",
@@ -16,7 +17,8 @@ const projectData = [
     {
         id: 2,
         title: "Infinitimac",
-        description : "3D Animation in Blender.November-December 2022.Solo project.This project involved creating a 4-second (100 frames) 3D animation using Blender software. Constraints were imposed: we had a base file that included an already animated camera that should not be modified and a circular object. The main element of our scene had to be a circular or spherical object of the same size as the one already present in the base file. Our instructor then compiled the work of all students to produce the video you see on the right.",
+        about: "3D Animation in Blender.November-December 2022.",
+        description : "Solo project.This project involved creating a 4-second (100 frames) 3D animation using Blender software. Constraints were imposed: we had a base file that included an already animated camera that should not be modified and a circular object. The main element of our scene had to be a circular or spherical object of the same size as the one already present in the base file. Our instructor then compiled the work of all students to produce the video you see on the right.",
         image: "/images/projects_view/forestnight2.png",
         tag: ["All", "3D", "Creation"],
         gitUrl: "/",
@@ -25,7 +27,8 @@ const projectData = [
     {
         id: 3,
         title: "Ratio",
-        description :"C++ Library for Rational Numbers. November-December 2022. Team of 2 people. Role: Creation of the library and mathematical reasoning. This project brings together skills acquired in C++ object-oriented programming and mathematics for computer science. We created a library containing a class for rational numbers (irreducible fractions) and overloaded mathematical operators to make these rational numbers easily usable by a user. We also implemented unit tests to verify that the functions in our library are correct.",
+        about: "C++ Library for Rational Numbers. November-December 2022.",
+        description :"Team of 2 people. Role: Creation of the library and mathematical reasoning. This project brings together skills acquired in C++ object-oriented programming and mathematics for computer science. We created a library containing a class for rational numbers (irreducible fractions) and overloaded mathematical operators to make these rational numbers easily usable by a user. We also implemented unit tests to verify that the functions in our library are correct.",
         image: "/images/projects_view/ratio.png",
         tag: ["All", "Software", "Maths"],
         gitUrl: "/",
@@ -34,7 +37,8 @@ const projectData = [
     {
         id: 4,
         title: "@eatude",
-        description: 'UX Design for a Student Assistance Application. November-December 2022. Team of 4 people. Role: Each stage of creation was done as a team. The central theme of this project was "How to encourage collaboration among students to help them eat better?" To better understand the needs, we conducted interviews with several individuals to create an affinity map. From this, we derived an experience map by defining a target (Persona) and user emotions. After a workshop to gather interesting ideas, we created initial sketches through User Flow and Wireframe before final design on Figma.',
+        about: "UX Design for a Student Assistance Application. November-December 2022.",
+        description: 'Team of 4 people. Role: Each stage of creation was done as a team. The central theme of this project was "How to encourage collaboration among students to help them eat better?" To better understand the needs, we conducted interviews with several individuals to create an affinity map. From this, we derived an experience map by defining a target (Persona) and user emotions. After a workshop to gather interesting ideas, we created initial sketches through User Flow and Wireframe before final design on Figma.',
         image: "/images/projects_view/ateatude.png",
         tag: ["All", "Web", "Design"],
         gitUrl: "/",
@@ -67,7 +71,7 @@ const ProjectsSection = () => {
             <ProjectCard 
                 key={project.id} 
                 title={project.title} 
-                description={project.description} 
+                description={project.about} 
                 imgUrl={project.image} 
                 gitUrl={project.gitUrl} 
                 previewUrl={project.prewiewUrl}/>))}
